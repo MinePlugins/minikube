2a7224efc5a3b2e384c2c382777ae4d8ba6ccf86
Louis BENOIST--FOUCHER <louis.benoist-foucher@estiam.com>
2020-02-24 09:48:42 +0100

    First version of minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+fi
+
+apt-get update
+apt-get install -y apt-transport-https
+apt-get upgrade
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+cp minikube /usr/local/bin && rm minikube
+
+minikube start --vm-driver=none
