ae0f6c37c93eaf17140df645bbd30a9ab5fb556e
Cournut <quentin.co@free.fr>
2020-02-24 13:58:06 +0100

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -30,3 +30,5 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-b
 cd minikube
 
 kubectl apply -f dashboard-adminuser.yaml
+
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
