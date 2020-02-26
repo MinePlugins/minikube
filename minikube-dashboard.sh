ec1658c7b1ba60961da9d1c50e952ce81e4e8608
Louis BENOIST--FOUCHER <louis.benoist-foucher@estiam.com>
2020-02-26 10:33:07 +0100

    Refacto

--- minikube-dashboard.sh
+++ minikube-dashboard.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Si on voulais mettre un custom certificates pour let's encrypt par exemple
 mkdir -p $HOME/certs
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out $HOME/certs/tls.crt -keyout $HOME/certs/tls.key
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out $HOME/certs/tls.crt -keyout $HOME/certs/tls.key -subj "/C=FR/ST=Paris/L=Paris/O=Global Security/OU=IT Department/CN=exemple.com"
+kubectl create namespace kubernetes-dashboard
 kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs -n kubernetes-dashboard
+
+# Création du dashboard
 kubectl apply -f https://raw.githubusercontent.com/MinePlugins/minikube/master/dashboard.yaml
+
+# Générer et afficher le token de login
+echo "Utiliser ce token d'accès : "
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | awk '/^deployment-controller-token-/{print $1}') | awk '$1=="token:"{print $2}'
